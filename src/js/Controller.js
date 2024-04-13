feed3c0119d13d81124fc9273f545b6a8adcc20f
Sephgat <102762384+Sephgat@users.noreply.github.com>
2024-04-12 19:26:31 -0700

    refactoring code

--- src/js/Controller.js
+++ src/js/Controller.js
@@ -1,22 +1,20 @@
 /** @jsx vNode */
-import { vNode, View } from "@ocdla/view"
-import * as TimerHtml from '../../dev_modules/@ocdla/timer/timerPageHTML.js';
-import Timer from '../../dev_modules/@ocdla/timer/timerObject.js';
+import { vNode, View } from "../../node_modules/@ocdla/view/view.js";
+import { TimerComponent } from '../../node_modules/@ocdla/timer/TimerComponent.js';
+import Timer from '../../node_modules/@ocdla/timer/Timer.js';
 
 
 //responable for createing action on the page
 class Controller {
-   
+
 
     static timer;
 
-    static createTimer() {
+    constructor() {
 
-        let html = View.createElement(<TimerHtml.mainHtmlComponent />);
-        document.getElementById("html").appendChild(html)
+        let html = View.createElement(<TimerComponent hours="0" minutes="00" seconds="00" />);
+        document.getElementById("html").appendChild(html);
 
-        html = View.createElement(<TimerHtml.mainButtonHtml />);
-        document.getElementById("html").appendChild(html)
 
     }
     static startTimer() {
@@ -24,31 +22,63 @@ class Controller {
         let minutes = document.getElementById('minutes');
         let seconds = document.getElementById('seconds');
         let cal = Timer.calcTimer(parseInt(hours.value), parseInt(minutes.value), parseInt(seconds.value));
-    
+
         this.timer = new Timer(cal);
-    
-        let html = View.createElement(<TimerHtml.TimerButtonHtml />)
-        let temp = document.getElementById("controls")
-        document.getElementById("html").replaceChild(html, temp);
-    
+        this.timer.onTick(function (h, m, s) {
+            let html = View.createElement(<TimerComponent hours={h} minutes={m} seconds={s} />);
+            let temp = document.getElementById("timer")
+            document.getElementById("html").replaceChild(html, temp);
+            Controller.addEventHandlers();
+        });
+
         this.timer.start();
 
+
+    }
+    static addEventHandlers() {
+        // Put the element on the page with an id of start in a variable
+        // Do the same for the stop button and the reset button
+    
+        let controls = document.getElementById("controls");
+        controls.addEventListener("click", Controller.handleEvent);
+    
     
     }
+    static handleEvent(e) {
+        //deturmine if what btn the user pressed
+        let target = e.target;
+        let action = target.dataset.action;
+        //let method = action + "Timer";
+        //this[method]()
+        if (action == "start") {
+            Controller.startTimer();
+            Controller.addEventHandlers();
+        }
+        if (action == "stop") {
+            Controller.stopTimer();
+            Controller.addEventHandlers();
+        }
+        if (action == "reset") {
+            //timer.stop();
+            Controller.resetTimer();
+            Controller.addEventHandlers();
+        }
+        
+    }
+    
+    
     static stopTimer() {
         this.timer.stop()
     }
-        
+
 
     static resetTimer() {
-    this.timer.stop();
-    let temp = document.getElementById("timer");
-    let html = View.createElement(<TimerHtml.mainHtmlComponent />);
-    document.getElementById("html").replaceChild(html, temp);
+        this.timer.stop();
+        let temp = document.getElementById("timer");
+        let html = View.createElement(<TimerComponent hours="0" minutes="00" seconds="00" />);
+        document.getElementById("html").replaceChild(html, temp);
 
-    temp = document.getElementById("controls");
-    html = View.createElement(<TimerHtml.mainButtonHtml />);
-    document.getElementById("html").replaceChild(html, temp);
+
 
     }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,9 @@
-import Controller from "./controller.js"
+import Controller from "./Controller.js"
 
 
-function addEventHandlers() {
-    // Put the element on the page with an id of start in a variable
-    // Do the same for the stop button and the reset button
 
-    let controls = document.getElementById("controls");
-    controls.addEventListener("click", handleEvent);
+let controller = new Controller();
+Controller.addEventHandlers();
 
 
-}
 
-function init(){
-    Controller.createTimer();
-    addEventHandlers();
-}
-
-function handleEvent(e) {
-    //deturmine if what btn the user pressed
-    let target = e.target;
-    let action = target.dataset.action;
-    //let method = action + "Timer";
-    //this[method]()
-    if (action == "start") {
-        Controller.startTimer();
-        addEventHandlers();
-    }
-    if (action == "stop") {
-        Controller.stopTimer();
-        addEventHandlers();
-    }
-    if (action == "reset") {
-        //timer.stop();
-        Controller.resetTimer();
-        addEventHandlers();
-    }
-    
-}
-init();
