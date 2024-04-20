e3043611837dcce107a468e4e4a009ae170c19c3
Sephgat <102762384+Sephgat@users.noreply.github.com>
2024-04-20 16:53:04 -0700

    fixed minor stuff

--- dev_modules/@ocdla/salesforceapi/salesforceapi.js
+++ dev_modules/@ocdla/salesforceapi/salesforceapi.js
@@ -6,13 +6,13 @@ class salesforceapi {
 
     constructor() {
         this.url = "https://ocdla-c-dev-ed.develop.my.salesforce.com/services/data/v60.0/query?q=SELECT name,id,hours__c,minutes__c,seconds__c from SavedTimer__c"
-        this.accessToken = "00Dbm000001u74U!AQEAQNvIjmLK1lEluIISnKlcJuEcdmN9KVSfhNpru67yYH86jG5_n0fE0GY0C7dAPKykes4Icvbq7v4WaVTalhQn2BVK0C3g";
+        this.accessToken = "00Dbm000001u74U!AQEAQOwxiLKiUdEm1zAC6MeefJgeX1wQvuSwhdAb_pXvF5sQbGsXUuw6Zc4uLu2IJXIbyG2xq1WOg3MwtlTNXwEQ8O7GjWwB";
+        this.myHeaders = new Headers();
     }
     async query() {
-        let myHeaders = new Headers();
-        myHeaders.append("Authorization", `Bearer ${this.accessToken}`);
+        this.myHeaders.append("Authorization", `Bearer ${this.accessToken}`);
         let fetchedData;
-        await fetch(`${this.url}`, {headers: myHeaders})
+        await fetch(`${this.url}`, {headers: this.myHeaders})
         .then(response => response.json())
         .then(data => { fetchedData = data;});
 
