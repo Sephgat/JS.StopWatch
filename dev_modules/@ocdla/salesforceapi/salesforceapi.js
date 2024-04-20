0ca82cd3def1acc434f48e8471e19820841679fb
Sephgat <102762384+Sephgat@users.noreply.github.com>
2024-04-19 18:51:35 -0700

    refactoring to a new name scheme

--- dev_modules/@ocdla/salesforceapi/salesforceapi.js
+++ dev_modules/@ocdla/salesforceapi/salesforceapi.js
@@ -6,9 +6,9 @@ class salesforceapi {
 
     constructor() {
         this.url = "https://ocdla-c-dev-ed.develop.my.salesforce.com/services/data/v60.0/query?q=SELECT name,id,hours__c,minutes__c,seconds__c from SavedTimer__c"
-        this.accessToken = "00Dbm000001u74U!AQEAQPolrYKl.ye4UosLEu2YLCEtpXXBpzi5eGhgRCSZtYbDw0D9Cu2xDpJJXTs9px3TYEhFsW3VjzQ9zCp1QYXnhEJAFmi3";
+        this.accessToken = "00Dbm000001u74U!AQEAQNvIjmLK1lEluIISnKlcJuEcdmN9KVSfhNpru67yYH86jG5_n0fE0GY0C7dAPKykes4Icvbq7v4WaVTalhQn2BVK0C3g";
     }
-    async fetchTimer() {
+    async query() {
         let myHeaders = new Headers();
         myHeaders.append("Authorization", `Bearer ${this.accessToken}`);
         let fetchedData;
