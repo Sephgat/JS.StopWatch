4f7efaa61485710a07c0959060fdc55d4868977a
Sephgat <102762384+Sephgat@users.noreply.github.com>
2024-04-24 19:32:34 -0700

    fixed minor spaning in the timer elements

--- dev_modules/@ocdla/Timer/TimerComponent.js
+++ dev_modules/@ocdla/Timer/TimerComponent.js
@@ -1,40 +1,39 @@
 /** @jsx vNode */
 
 
-import {vNode, View} from "../view/view.js";
+import { vNode, View } from "../view/view.js";
 
 
 
-export const TimerComponent = function(props) {
+export const TimerComponent = function (props) {
 
     let h = props.hours.toString();
     let m = props.minutes.toString();
     let s = props.seconds.toString();
 
     return (
-        <div class="timer h1 p-5 mt-4  text-center bg-light rounded"  id="timer">
+        <div class="timer h1 p-5 mt-4  text-center bg-light rounded" id="timer">
             <div>
-                    <input type="number" id="hours" value={h} style="width: 75px;"/>:
-                    <input type="number" id="minutes" value={m} style="width: 75px;"/> : 
-                    <input type="number" id="seconds" value={s} style="width: 75px;"/>
+                <input type="number" id="hours" value={h} style="margin: 5px; width: 75px;" />:
+
+                <input type="number" id="minutes" value={m} style="margin: 5px; width: 75px;" />:
+
+                <input type="number" id="seconds" value={s} style="margin: 5px; width: 75px;" />
             </div>
-            <div id="controls">
-                <div class="container text-center">
-                    <Button action="start" id="start" label="start"/>
-                    <Button action="stop" id="stop" label="stop"/>
-                    <Button action="reset" id="reset" label="reset"/>
-                </div>
-            </div>
+
+            <Button action="start" id="start" class="p-15" label="start" />
+            <Button action="stop" id="stop" class="p-15" label="stop" />
+            <Button action="reset" id="reset" class="m-5" label="reset" />
         </div>
     );
 };
 
-const Button = function(props) {
+const Button = function (props) {
 
     // convert string of keys to an array of characters.
 
     return (
-                <button class="btn btn-lg btn-success" data-action={props.action} id={props.id}>{props.label}</button>
+        <button class="btn btn-lg btn-success" data-action={props.action} id={props.id}>{props.label}</button>
     );
 
 };
